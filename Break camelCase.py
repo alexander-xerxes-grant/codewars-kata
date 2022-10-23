91cc7eefd5649d74084e48c61e1c72f4843494ea
Alexander Grant <alexander.grant@dunelm.com>
2022-10-23 21:01:31 +0100

    complete 'Break camelCase'

---
+++ Break camelCase.py
@@ -0,0 +1,9 @@
+def solution(s):
+    result = ''
+    
+    for char in s:
+        if char.isupper() and char != s[0]:
+            result += " " + char
+        else:
+            result += char
+    return result
