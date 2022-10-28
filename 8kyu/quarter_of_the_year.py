962a43bf1a6af673c48af53aaef7555c38b51045
Alexander Grant <alexander.grant@dunelm.com>
2022-10-28 07:56:03 +0100

    refactor quarter_of_the_year kata to use math.ceil()

--- 8kyu/quarter_of_the_year.py
+++ 8kyu/quarter_of_the_year.py
@@ -2,23 +2,13 @@
 
 # For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 
+from math import ceil
+
 def quarter_of(month):
-    year = [m for m in range(1, 13)]
-    # print(year)
-    Q1 = year[:3]
-    Q2 = year[3:6]
-    Q3 = year[6:9]
-    Q4 = year[9:12]
+    return ceil(month/3)
 
-    if month in Q1:
-        return 1
-    elif month in Q2:
-        return 2
-    elif month in Q3:
-        return 3
-    else:
-        return 4
-
+print(quarter_of(1))
+print(quarter_of(2))
 print(quarter_of(3))
 print(quarter_of(8))
 print(quarter_of(11))
