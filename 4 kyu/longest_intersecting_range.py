ee90224e9647c961f4bdddfa2d26890a516c5b9c
Alexander Grant <alexander.grant@dunelm.com>
2022-11-17 08:31:53 +0000

    Improve sum of intervals solution

--- 4 kyu/longest_intersecting_range.py
+++ 4 kyu/longest_intersecting_range.py
@@ -66,22 +66,22 @@
 data = [[1, 2], [6, 10], [11, 15]]
 data8 = [(1, 5), (6, 10)]
 data5 = [(1, 5)]
+data19 = ([1,5], [10, 20], [1, 6], [16, 19], [5, 11])
 
 
 
-def sum_of_intervals(array_of_intervals):
+def sum_of_intervals(intervals):
+    s, top = 0, float("-inf")
+    for a,b in sorted(intervals):
+        if top < a: top    = a
+        if top < b: s, top = s+b-top, b
+    return s
+    
 
-    # # Create new array of intervals combined
 
-    # intervals = []
-
-    # for pair in array_of_intervals:
-    #     for index in range(pair[0], (pair[1])):
-    #         intervals.append(index)
-    # return len(set(intervals))
-    return len(set({i for a,b in array_of_intervals for i in range(a,b)}))
 
 
 print(sum_of_intervals(data), "should be 9")
 print(sum_of_intervals(data5), "should be 4")
 print(sum_of_intervals(data8), "should be 8")
+print(sum_of_intervals(data19), "should be 19")
