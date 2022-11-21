57b005dcffbbe5268e2d124fb80b406f3e5f1025
Alexander Grant <alexander.grant@dunelm.com>
2022-11-21 16:29:45 +0000

    min_max kata

---
+++ 7kyu/min_max.py
@@ -0,0 +1,14 @@
+def min_max(array):
+    return [min(array), max(array)]
+
+
+
+example_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+example_array2 = [2334454, 5]
+example_array3 = [1]
+example_array4 = [1,1]
+
+print(min_max(example_array))
+print(min_max(example_array2))
+print(min_max(example_array3))
+print(min_max(example_array4))
