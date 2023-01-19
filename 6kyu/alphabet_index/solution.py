e0c76c4c439373a90d5a4daad6a515ba10ef4046
Alexander Grant <alexander.grant@dunelm.com>
2023-01-19 16:48:20 +0000

    6kyu - alphabet_index solution and tests.

---
+++ 6kyu/alphabet_index/solution.py
@@ -0,0 +1,8 @@
+import string
+
+alphabet = string.ascii_lowercase
+
+def get_index(s):
+    
+    s = s.lower()
+    return " ".join([str(alphabet.index(letter) + 1) for letter in s if letter.isalpha()])
---
+++ 6kyu/alphabet_index/test_solution.py
@@ -0,0 +1,9 @@
+from solution import get_index
+
+
+def test_get_index():
+    
+    assert get_index("The sunset sets at twelve o' clock.") == "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+    assert get_index("abc") == "1 2 3"
+    assert get_index("a.b.c") == "1 2 3"
+    
