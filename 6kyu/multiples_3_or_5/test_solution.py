32d3207950674c48bc45ae876ecdb3032db10482
Alexander Grant <alexander.grant@dunelm.com>
2023-01-19 16:46:53 +0000

    multiples_3_or_5

---
+++ 6kyu/multiples_3_or_5/test_solution.py
@@ -0,0 +1,10 @@
+from solution import find_multiples
+
+
+def test_find_multiples():
+    
+    assert find_multiples(10) == 23
+    assert find_multiples(0) == 0
+    assert find_multiples(4) == 3
+    assert find_multiples(6) == 8
+    assert find_multiples(16) == 60
