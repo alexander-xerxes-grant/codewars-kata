dedc57d3396f1b79bc921550394814fcea49a624
Alexander Grant <alexander.grant@dunelm.com>
2023-05-18 06:56:18 +0100

    valid anagram kata practice.

--- leetcode/easy/valid_anagram/solution.py
+++ leetcode/easy/valid_anagram/solution.py
@@ -3,26 +3,21 @@ from collections import Counter
 
 class Solution:
     def is_anagram(self, s: str, t: str) -> bool:
-        
-        # check if the string is an anagram of the target
-        # s and t must be the same length
-        # create two dicts (hashmaps)
-        # these will be the counts of the number of each
-        # letter in each string
-        
-        # then iterate over the dicts and check
-        # each count of each letter is equal
-        
         if len(s) != len(t):
             return False
+
+        s_map = {}
+        t_map = {}
+
+        for c in s:
+            s_map[c] = 1 + s_map.get(c, 0)
+        for x in t:
+            t_map[x] = 1 + t_map.get(x, 0)
         
-        count_s, count_t = {}, {}
-        
-        for i in range(len(s)):
-            count_s[s[i]] = 1 + count_s.get(s[i], 0)
-            count_t[t[i]] = 1 + count_t.get(t[i], 0)
-        
-        for c in count_s:
-            if count_s[c] != count_t.get(c, 0):
+        for a in t_map.keys():
+            if t_map.get(a, 0) != s_map.get(a, 0):
+                print(a)
                 return False
-        return True
+            else:
+                return True
+        
