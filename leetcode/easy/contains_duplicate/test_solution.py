073e02341548ea29d61c2528691360ee84a86003
Alexander Grant <alexander.grant@dunelm.com>
2023-07-06 08:08:53 +0100

    Solve contains_duplicate.

--- leetcode/easy/contains_duplicate/test_solution.py
+++ leetcode/easy/contains_duplicate/test_solution.py
@@ -10,6 +10,7 @@ class TestSolution:
         return Solution()
     
     def test_contains_duplicate(self, solution):
-        assert solution.contains_duplicate([1, 2, 3, 1]) == True
-        assert solution.contains_duplicate([1, 2, 3, 4]) == False
-        assert solution.contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
+        assert solution.contains_duplicate([1, 2, 1]) == True
+        assert solution.contains_duplicate([1, 2, 3]) == False
+        assert solution.contains_duplicate([1]) == False
+
