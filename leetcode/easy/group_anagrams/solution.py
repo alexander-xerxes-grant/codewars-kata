6c16db50aac53d928f83fb0f51875d4875bf5540
Alexander Grant <alexander.grant@dunelm.com>
2023-08-01 16:23:02 +0100

    Group anagrams practice.

--- leetcode/easy/group_anagrams/solution.py
+++ leetcode/easy/group_anagrams/solution.py
@@ -1,11 +1,20 @@
+from collections import defaultdict
 from typing import List
 
-
-
-
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 
 
 class Solution:
-    
     def group_anagrams(self, strs: List[str]) -> List[List[str]]:
-        
+        result = defaultdict(list)
+
+        for s in strs:
+
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            result[tuple(count)].append(s)
+
+        return list(result.values())
