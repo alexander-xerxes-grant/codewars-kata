e3a6a1f74c869717a628634f22a6a94bbbb33f6d
Alexander Grant <alexander.grant@dunelm.com>
2024-01-11 12:08:25 +0000

    Code every day.

--- leetcode/easy/valid_anagram/solution.py
+++ leetcode/easy/valid_anagram/solution.py
@@ -1,16 +1,3 @@
 class Solution:
     def is_anagram(self, a: str, b: str) -> bool:
-        if len(a) != len(b):
-            return False
-        
-        count_a, count_b = {}, {}
-        
-        for i in range(len(a)):
-            count_a[a[i]] = 1 + count_a.get(a[i], 0)
-            count_b[b[i]] = 1 + count_b.get(b[i], 0)
-            
-        for c in count_a:
-            if count_a[c] != count_b.get(c, 0):
-                return False
-        
-        return True
+        pass
